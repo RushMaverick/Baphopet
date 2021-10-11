04b061d7d14928e70ae1edf061d37a22665c426d
RushMaverick <rasse.rask@gmail.com>
2021-10-11 16:53:09 +0300

    PlayerPunch hit detection added
    Thanks Brackeys

--- Baphopet/Assets/Scripts/PlayerCombat.cs
+++ Baphopet/Assets/Scripts/PlayerCombat.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    public Animator anim;
+
+    public Transform punchPoint; 
+    public float punchRange = 10f;
+    public LayerMask punchLayers;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +27,21 @@ public class PlayerCombat : MonoBehaviour
     void Punch()
     {
         //Play an attack animation
+        anim.SetTrigger("punch");
         //Detect what enemies are in the hitbox
+        Collider2D[] hitPunch = Physics2D.OverlapCircleAll(punchPoint.position, punchRange, punchLayers);
         //Set enemy to stunned
+        foreach(Collider2D punch in hitPunch)
+        {
+            Debug.Log("Stunned " + punch.name);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (punchPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(punchPoint.position, punchRange);    
     }
 }
