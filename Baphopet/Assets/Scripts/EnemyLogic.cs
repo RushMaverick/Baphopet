050b44044eacc638fdf03e501d05bed8c56f0e44
RushMaverick <rasse.rask@gmail.com>
2021-10-16 20:27:31 +0300

    Knockback added

--- Baphopet/Assets/Scripts/EnemyLogic.cs
+++ Baphopet/Assets/Scripts/EnemyLogic.cs
@@ -6,25 +6,39 @@ public class EnemyLogic : MonoBehaviour
 {
     public Animator anim;
     public bool stunned;
-
+    private Transform Captor; //Targets where Player is
     public int HitsLeft = 1;
     int CurrentHits;
     // Start is called before the first frame update
     void Start()
     {
+        Captor = FindObjectOfType<PlayerMovement>().transform; //Location of Player
         stunned = false;
         CurrentHits = HitsLeft;
+        Debug.Log("Player is" + Captor);
     }
+
 
     public void GetPunched(int hits)
         {
             CurrentHits -= hits;
+            KnockedBack();
+
             if(CurrentHits <= 0)
                 {
                     stunned = true;
                 }
         }
-
+    
+    public void KnockedBack()
+        {
+            ////Play Stunned Animation
+            anim.SetTrigger("Stun");
+            Debug.Log("Knocked Back");
+            //Knockback
+            Vector2 difference = transform.position - Captor.transform.position;
+            transform.position = new Vector2(transform.position.x + difference.x, transform.position.y + difference.y);
+        }
     public void Captured()
         {
             Debug.Log("Demon captured!");
