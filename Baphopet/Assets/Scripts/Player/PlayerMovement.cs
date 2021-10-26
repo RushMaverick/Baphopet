ad103218da0b6e205645b3b2f665cd84632bc2b2
Lazyfella <alexander.tallgren@gmail.com>
2021-10-26 12:39:45 +0300

    Movement animations with scuffed sprites
    Need more sprites

--- Baphopet/Assets/Scripts/Player/PlayerMovement.cs
+++ Baphopet/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,31 +8,31 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
-    
+
     // Update is called once per frame
     void Update()
     {
-        // Input
-        /*movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        */
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
-        {
-        movement.y = 0;
-        }
-        else
-        {
-        movement.x = 0;
-        }
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
+        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+        {
+            movement.y = 0;
+        }
+        else
+        {
+            movement.x = 0;
+        }
         // Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
