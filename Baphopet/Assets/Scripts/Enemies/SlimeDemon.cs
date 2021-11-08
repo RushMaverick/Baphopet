7d6007f278b9c5e2d6ec18a1d5ac64f38c04bb30
RushMaverick <rasse.rask@gmail.com>
2021-11-08 19:46:55 +0200

    Demon chases player

---
+++ Baphopet/Assets/Scripts/Enemies/SlimeDemon.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeDemon : Enemy
+{
+    public Transform target;
+    public float escapeRadius;
+    public float safeRadius; 
+    public float moveSpeed;
+    public Transform homePosition; 
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        Debug.Log("Target is " + target.name);
+    }
+
+   
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= escapeRadius && Vector3.Distance(target.position, transform.position) > safeRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+
+
+    }
+
+       void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.position, escapeRadius);
+        Gizmos.DrawWireSphere(transform.position, safeRadius);
+    }
+}
